71b09437fd32f5b62e954b98d0dcf21b1c84c8ac
Ben Oxley <ben.oxley@gmail.com>
2018-06-02 16:05:51 +0100

    Split solutions into separate ones for Linux, OSX and Windows respectively

---
+++ GNU.Plot.Linux/PlotRenderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GNU.Plot
+{
+    public static class PlotRenderExtensions
+    {
+        public static Image AsImage(this PlotRender d)
+        {
+            return Image.FromFile(d.Output.FullName);
+        }
+    }
+}
